4c2cb1d05277987e5ba66f3511a3636b1cd44930
James Cuzella <james.cuzella@lyraphase.com>
2023-09-09 10:52:53 -0600

    Examples: Refactoring with Tuples & Structs

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@
  */
 
 /// # A short program with one way of calculating the area of a rectangle
-/// 
+///
 /// Take the width and height of a rectangle specified in pixels and calculate
-/// the area of the rectangle. 
+/// the area of the rectangle.
 fn main() {
     let width1 = 30;
     let height1 = 50;
@@ -16,6 +16,12 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area_simple(width1, height1)
     );
+
+    // Refactoring with Tuples
+    tuples();
+
+    // Refactoring with Structs: Adding More Meaning
+    structs();
 }
 
 /// # Simple Area function
@@ -23,4 +29,56 @@ fn main() {
 /// `height` variables
 fn area_simple(width: u32, height: u32) -> u32 {
     width * height
+}
+
+/// # Refactoring with Tuples
+///
+/// Another version of our program that uses tuples.
+fn tuples() {
+    let rect1 = (30, 50);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_tuples(rect1)
+    );
+}
+
+/// # Calculate are of a rectangle with Tuples
+///
+/// Refactored area function using tuples
+fn area_tuples(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+// Refactoring with Structs: Adding More Meaning
+/// # Rectangle struct to group `height` and `width` data
+/// 
+/// We use structs to add meaning by labeling the data.
+/// We can transform the tuple we’re using into a struct with a name for
+/// the whole as well as names for the parts, as shown this struct.
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+/// # Refactoring with Structs: Adding More Meaning
+///
+/// Another version of our program that uses structs
+fn structs() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_structs(&rect1)
+    );
+}
+
+/// # Calculate area of a rectangle with Structs
+/// 
+/// Refactored area function using structs
+fn area_structs(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
